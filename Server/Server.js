4df8c3b9cbcad2fa8bf483293d34551806b315c1
Ayush Ojha <117572529+Ayushojhabxr@users.noreply.github.com>
2025-08-20 11:31:36 +0530

    Update Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://task-lytic.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
@@ -52,3 +52,4 @@ app.use('/api/posts', forumPostRoutes);
 app.listen(port, () => {
   console.log(` Server running on http://localhost:${port}`);
 });
+
