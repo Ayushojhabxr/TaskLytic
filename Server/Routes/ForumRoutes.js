8d95dc5f921e79148c3b1537ff3421ecc7f393e5
Ayush Ojha <117572529+Ayushojhabxr@users.noreply.github.com>
2025-08-21 10:50:25 +0530

    Update ForumRoutes.js

--- Server/Routes/ForumRoutes.js
+++ Server/Routes/ForumRoutes.js
@@ -1,14 +1,15 @@
-// routes/forumPostRoutes.js
+
 import express from 'express';
 import { createForumPost, getAllForumPosts } from '../Controllers/forumController.js';
 import {isAuthenticated} from '../Middleware/authuser.js';
 
 const router = express.Router();
 
-// 👇 POST request to create a new forum post
+//  POST request to create a new forum post
 router.post('/create', isAuthenticated, createForumPost);
 
-// 👇 GET request to fetch all forum posts
+// GET request to fetch all forum posts
 router.get('/get', isAuthenticated, getAllForumPosts);
 
 export default router;
+
